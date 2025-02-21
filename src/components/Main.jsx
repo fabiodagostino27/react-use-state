78fa85800e62164495496b3dc69db6eb8e94ed56
fabiodagostino27 <fabiodago27@gmail.com>
2025-02-21 15:18:02 +0100

    ADD: useState

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,20 @@
-import { ButtonsList } from "./buttons/ButtonsList"
+import { useState } from "react";
+import { ButtonsList } from "./buttons/ButtonsList";
 
 export const Main = (props) => {
+    const [currentId, setCurrentId] = useState(1);
+    const languages = props.languages;
+    const {description} = languages;
+
+    const currentObject = languages.find(element => element.id === currentId);
     return (
         <div className="container mt-3">
             <div>
-                <ButtonsList languages={props.languages}/>
+                <ButtonsList languages={languages}/>
             </div>
+            <p>
+                {!currentObject ? "Nessun linguaggio selezionato" : currentObject.description}
+            </p>
         </div>
     )
 }
